88c57f13f95b978d862cc2008eaafd99428d248c
Jérémie P <jpourageaux@hotmail.fr>
2025-03-26 15:59:35 +0100

    first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    /**
+     * Ma première ligne de commentaire pour javadoc
+     * ma deuxième ligne
+     * ma troisième ligne
+     */
+    public static void main(String[] args) {
+        System.out.println("Coucou");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
+
